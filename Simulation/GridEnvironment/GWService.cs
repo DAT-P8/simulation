9eaefa5de3388cce30d54e24b57b5c7dd103e90d
Mads Byriel <madshbyriel@gmail.com>
2026-02-23 17:38:07 +0100

    First instantiation of a 'drone'

--- Simulation/GridEnvironment/GWService.cs
+++ Simulation/GridEnvironment/GWService.cs
@@ -8,26 +8,26 @@ using Serilog;
 namespace Simulation.GridEnvironment;
 
 public class GWService(
-    ILogger logger
+	ILogger logger
 ) : GWSimulation.GWSimulation.GWSimulationBase
 {
-    private readonly ILogger _logger = logger;
+	private readonly ILogger _logger = logger;
 
-    public override Task<GWCloseResponse> Close(GWCloseRequest request, ServerCallContext context)
-    {
-        _logger.Information("Close: {Request}", request);
-        throw new NotImplementedException();
-    }
+	public override Task<GWCloseResponse> Close(GWCloseRequest request, ServerCallContext context)
+	{
+		_logger.Information("Close: {Request}", request);
+		throw new NotImplementedException();
+	}
 
-    public override Task<GWActionResponse> DoStep(GWActionRequest request, ServerCallContext context)
-    {
-        _logger.Information("DoStep: {Request}", request);
-        throw new NotImplementedException();
-    }
+	public override Task<GWActionResponse> DoStep(GWActionRequest request, ServerCallContext context)
+	{
+		_logger.Information("DoStep: {Request}", request);
+		throw new NotImplementedException();
+	}
 
-    public override Task<GWResetResponse> Reset(GWResetRequest request, ServerCallContext context)
-    {
-        _logger.Information("Reset: {Request}", request);
-        throw new NotImplementedException();
-    }
+	public override Task<GWResetResponse> Reset(GWResetRequest request, ServerCallContext context)
+	{
+		_logger.Information("Reset: {Request}", request);
+		throw new NotImplementedException();
+	}
 }
--- Simulation/Main.cs
+++ Simulation/Main.cs
@@ -8,18 +8,23 @@ namespace Simulation;
 
 public partial class Main : Node3D
 {
+	private readonly PackedScene _droneScene = GD.Load<PackedScene>("res://gw_drone.tscn");
+
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-        var logger = new LoggerConfiguration()
-            .WriteTo.Console()
-            .CreateLogger();
-        Log.Logger = logger;
+		var logger = new LoggerConfiguration()
+			.WriteTo.Console()
+			.CreateLogger();
+		Log.Logger = logger;
 
-        var gwService = new GWService(logger);
+		var gwService = new GWService(logger);
 
-        var server = new Server(logger, "localhost", 50051, gwService);
-        server.StartServer();
+		var server = new Server(logger, "localhost", 50051, gwService);
+		server.StartServer();
+
+		var drone = _droneScene.Instantiate<StaticBody3D>();
+		AddChild(drone);
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.

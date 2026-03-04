b2cb675463e4d9ecfefe7cb640bdb821357fbdb7
Mads Byriel <madshbyriel@gmail.com>
2026-03-04 08:56:00 +0100

    WIP

---
+++ Simulation/quick_dirty_tests/3d.py
@@ -0,0 +1,46 @@
+from typing import Iterable
+import TDF_pb2_grpc as gmodels
+from TDF_pb2 import (
+    TDFCloseRequest,
+    TDFCloseResponse,
+    TDFDoStepRequest,
+    TDFDoStepResponse,
+    TDFDroneAction,
+    TDFNewRequest,
+    TDFNewResponse,
+    TDFResetRequest,
+    TDFResetResponse
+)
+import grpc
+
+class Client:
+    def __init__(self, channel: grpc.Channel) -> None:
+        self.client = gmodels.TDFSimulationStub(channel)
+
+    def DoStep(self, id: int, actions: Iterable[TDFDroneAction]) -> TDFDoStepResponse:
+        return self.client.DoStep(TDFDoStepRequest(id=id, drone_actions=actions))
+
+    def New(self) -> TDFNewResponse:
+        return self.client.New(TDFNewRequest())
+
+    def Reset(self, id: int) -> TDFResetResponse:
+        return self.client.Reset(TDFResetRequest(id=id))
+
+    def Close(self, id: int) -> TDFCloseResponse:
+        return self.client.Close(TDFCloseRequest(id=id))
+
+class Simulation:
+    def __init__(self, client: Client) -> None:
+        self.client = client
+        response = self.client.New()
+        if response.WhichOneof("error_case") == "error_msg":
+            raise Exception(f"Received error response: {response.error_msg}")
+        if response.state.terminated:
+            raise Exception("Simulation terminated at initialization")
+
+        self.id = response.state.sim_id
+        self.drones = [d for d in response.state.drone_states]
+
+    def Progress(self):
+
+        pass
--- Simulation/quick_dirty_tests/build.sh
+++ Simulation/quick_dirty_tests/build.sh
@@ -5,3 +5,5 @@ set -e
 cd "$(dirname $0)"
 
 python -m grpc_tools.protoc -I../../Protos --python_out=. --pyi_out=. --grpc_python_out=. ../../Protos/grid_world.proto
+
+python -m grpc_tools.protoc -I../../Protos --python_out=. --pyi_out=. --grpc_python_out=. ../../Protos/TDF.proto

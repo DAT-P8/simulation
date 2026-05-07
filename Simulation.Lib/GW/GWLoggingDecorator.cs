98914fe0281261e0b71e635d827605a3a36fdd74
Mads Byriel <madshbyriel@gmail.com>
2026-05-07 11:41:56 +0200

    fix: better logging

--- Simulation.Lib/GW/GWLoggingDecorator.cs
+++ Simulation.Lib/GW/GWLoggingDecorator.cs
@@ -11,27 +11,39 @@ public class GWLoggingDecorator(
     private readonly ILogger _logger = logger;
     private readonly SimulationService.SimulationServiceBase _inner = inner;
 
-    public override Task<CloseResponse> Close(CloseRequest request, Grpc.Core.ServerCallContext context)
+    public async override Task<CloseResponse> Close(CloseRequest request, Grpc.Core.ServerCallContext context)
     {
-        _logger.Debug("GW Close: {Request}", request);
-        return _inner.Close(request, context);
+        _logger.Debug("GW Close Request: {Id}", request.SimId);
+        var r = await _inner.Close(request, context);
+        _logger.Debug("GW Close Response: {Id}", request.SimId);
+
+        return r;
     }
 
-    public override Task<DoStepResponse> DoStep(DoStepRequest request, Grpc.Core.ServerCallContext context)
+    public async override Task<DoStepResponse> DoStep(DoStepRequest request, Grpc.Core.ServerCallContext context)
     {
-        _logger.Debug("GW DoStep: {Request}", request);
-        return _inner.DoStep(request, context);
+        _logger.Debug("GW DoStep Request: {Id}", request.SimId);
+        var r = await _inner.DoStep(request, context);
+        _logger.Debug("GW DoStep Response: {Id}", request.SimId);
+
+        return r;
     }
 
-    public override Task<NewResponse> New(NewRequest request, Grpc.Core.ServerCallContext context)
+    public async override Task<NewResponse> New(NewRequest request, Grpc.Core.ServerCallContext context)
     {
-        _logger.Debug("GW New: {Request}", request);
-        return _inner.New(request, context);
+        _logger.Debug("GW New Request");
+        var r = await _inner.New(request, context);
+        _logger.Debug("GW New Response");
+
+        return r;
     }
 
-    public override Task<ResetResponse> Reset(ResetRequest request, Grpc.Core.ServerCallContext context)
+    public async override Task<ResetResponse> Reset(ResetRequest request, Grpc.Core.ServerCallContext context)
     {
-        _logger.Debug("GW Reset: {Reset}", request);
-        return _inner.Reset(request, context);
+        _logger.Debug("GW Reset Request: {Id}", request.SimId);
+        var r = await _inner.Reset(request, context);
+        _logger.Debug("GW Reset Response: {Id}", request.SimId);
+
+        return r;
     }
 }
